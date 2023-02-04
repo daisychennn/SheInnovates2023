f4dcb4b20d2b59dc70cc2b90033c27fa8cffff0b
daisychennn <dac229@pitt.edu>
2023-02-04 15:16:53 -0500

    read in dataset

--- map.py
+++ map.py
@@ -3,9 +3,13 @@ import folium
 import numpy as np
 import imageio
 import cv2
+import requests
+import io
 
-df = pd.read_csv("https://raw.githubusercontent.com/daisychennn/SheInnovates2023/daisychennn-dataset-1-1/Pitt%20Dining.csv")
-df.head()
+url = "https://raw.githubusercontent.com/daisychennn/SheInnovates2023/main/Pitt-Dining.csv"
+r = requests.get(url)
+# print(r.text)
+df = pd.read_csv(io.StringIO(r.text))
 
 
 m = folium.Map(location=[40.4426, -79.9564], tiles='Stamen Terrain', zoom_start=16.5, control_scale = True)
